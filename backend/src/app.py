bcbafa6568cf2ae324ef5a22d04ef2e89e19bd2d
sunnyunde-xooa <Sunny.Unde@xooa.com>
2022-06-11 18:58:38 +0530

    POST request

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,9 +4,10 @@ sys.path.insert(0, './utils')
 from authenticate import authenticateUser
 app = Flask(__name__)
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register(user):
+    user = request.get_json(force=True)
     authenticateUser(user)
-    if(!)
+    return 200
 
 app.run(host='0.0.0.0',port=8080)
